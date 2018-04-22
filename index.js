80aaa7e3ba724cb2d2d98c121da1f504749dc8bb
chunkydonut21 <shivammahe21@gmail.com>
2018-04-22 15:57:52 +0530

    changed greeting

--- index.js
+++ index.js
@@ -3,8 +3,32 @@ const app = express();
 
 //incoming request, outgoing response
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+//initial deployment
+//git --version
+//git init
+//git add .
+//git commit -m "initial commit"
+
+//subsequent deployment
+//git status -> to check the no. of files changed
+
+//after installing heroku cli use heroku -v to check cli version
+//heroku login
+//heroku create
+//git remote add https://git.heroku.com/ancient-waters-89030.git
+//git push heroku master
+//heroku open
+//heroku logs
+
+//mkdir server
+//cd server
+//npm init
+//npm install --save express
+//atom .
+//node index.js
