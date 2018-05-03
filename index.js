db2c3ef47c47e17424ea36a4a375622908d14953
chunkydonut21 <shivammahe21@gmail.com>
2018-05-03 19:17:09 +0530

    files changed

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
 const express = require('express');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 
-//incoming request, outgoing response
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
-//initial deployment
-//git --version
-//git init
-//git add .
-//git commit -m "initial commit"
+//incoming request, outgoing response
+// app.get('/', (req, res) => {
+//   res.send({ bye: 'buddy' });
+// });
 
-//subsequent deployment
-//git status -> to check the no. of files changed
+
+
+//heroku deployment
+/**
+ * 1. dynamic port binding
+ * 2. specify node environment
+ * 3. specify start Script
+ * 4. create .gitignore file
+ */
+
+//initial deployment
+/**
+ * git --version
+ * git init
+ * git add .
+ * git commit -m "initial commit"
+ */
 
 //after installing heroku cli use heroku -v to check cli version
-//heroku login
-//heroku create
-//git remote add https://git.heroku.com/ancient-waters-89030.git
-//git push heroku master
-//heroku open
-//heroku logs
+/**
+ * heroku login
+ * heroku create
+ * git remote add heroku https://git.heroku.com/ancient-waters-89030.git
+ * git push heroku master
+ * heroku open
+ * heroku logs
+ */
+
+//subsequent deployment
+/**
+ * git status -> to check the no. of files changed
+ * git add .
+ * git commit -m "files changed"
+ * git push heroku master
+ */
 
 //mkdir server
 //cd server
@@ -32,3 +56,5 @@ app.listen(PORT);
 //npm install --save express
 //atom .
 //node index.js
+
+//npm install --save passport passport-google-oauth20
