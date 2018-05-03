f36b13399aac92d02c09d7ea5718467d6f2fa5f5
chunkydonut21 <shivammahe21@gmail.com>
2018-05-03 19:24:17 +0530

    files changed

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 //incoming request, outgoing response
-// app.get('/', (req, res) => {
-//   res.send({ bye: 'buddy' });
-// });
+app.get('/', (req, res) => {
+  res.send({ bye: 'buddy' });
+});
 
 
 
