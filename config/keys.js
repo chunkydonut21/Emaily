eda08d609d5a20ea38d72fa9bb662db9e24c1eeb
chunkydonut21 <shivammahe21@gmail.com>
2018-05-09 21:19:53 +0530

    Added Billing and Client Side

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
-//kes.js - figure out which set of credentials to return
+//key.js - figure out which set of credentials to return
 
-if(process.env.NODE_ENV == 'production') {
+if(process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else {
   module.exports = require('./dev');
