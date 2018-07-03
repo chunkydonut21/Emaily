597a9d071fb5fb76196cf4f59bfe82c25eccad31
chunkydonut21 <shivammahe21@gmail.com>
2018-07-03 18:08:16 +0530

    dsds

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -18,7 +18,6 @@ class App extends React.Component {
       <BrowserRouter>
         <div className="container">
           <Header />
-          gg
           <Route exact path="/" component={Landing} />
           <Route exact path="/surveys" component={Dashboard} />
           <Route path="/surveys/new" component={SurveyNew} />
